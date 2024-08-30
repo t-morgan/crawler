993c4cb0fb5ba8acf6b450f1d801ca3b2e793737
t-morgan <tim@immutableholdings.com>
2024-08-30 11:56:05 -0400

    main args

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+
+	if len(args) < 1 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+
+	if len(args) > 1 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	fmt.Printf("starting crawl of: %v\n", args[0])
 }
